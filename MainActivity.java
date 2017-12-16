48dc6e3e07d2ef297917a060bde9a4a29ab56d60
koushik512 <bpavankoushik@gmail.com>
2017-12-16 22:06:04 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,28 +1,90 @@
-package com.example.vamsee.fgh;
+package com.example.lenovo.camera;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-     Button goButton;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+  
+public class MainActivity extends Activity {
+
+    private ImageView imageHolder;
+    private final int requestCode = 20;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        goButton=(Button)findViewById(R.id.goButton);
-        goButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent i=new Intent(MainActivity.this,secondMain.class);
-                startActivity(i);
+
+        imageHolder = (ImageView)findViewById(R.id.captured_photo);
+        Button capturedImageButton = (Button)findViewById(R.id.photo_button);
+        capturedImageButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(photoCaptureIntent, requestCode);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(this.requestCode == requestCode && resultCode == RESULT_OK){
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+
+           String partFilename = currentDateFormat();
+            storeCameraPhotoInSDCard(bitmap, partFilename);
+
+            // display the image from SD Card to ImageView Control
+           String storeFilename = "photo_" + partFilename + ".jpg";
+           Bitmap mBitmap = getImageFileFromSDCard(storeFilename);
+            imageHolder.setImageBitmap(mBitmap);
+        }
+    }
+
+    private String currentDateFormat(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String  currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
+    }
+
+    private void storeCameraPhotoInSDCard(Bitmap bitmap, String currentDate){
+        File outputFile = new File(Environment.getExternalStoragePublicDirectory("Myfolder1"), "photo_" + currentDate + ".jpg");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private Bitmap getImageFileFromSDCard(String filename){
+        Bitmap bitmap = null;
+        File imageFile = new File(Environment.getExternalStoragePublicDirectory("MYfolder1") + filename);
+        try{
+            FileInputStream fis = new FileInputStream(imageFile);
+            bitmap = BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
